74684a4cbd220f47ee8ca5a0686d374f511c3122
Marius Debussche <marius.debussche@gmail.com>
2026-04-19 17:37:58 +0200

    per channels sessions

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,85 @@
-use mseq::{Instruction, MSeqError, MidiInParam, MidiMessage, MidiNote};
+mod session;
+use self::session::*;
+
+use std::fmt::Display;
+
+use log::{debug, info};
+use mseq::{Instruction, MSeqError, MidiInParam, MidiMessage};
 use thiserror::Error;
 
-// todo: stop all notes still playing
-// maybe live note analysis is worth it after all
-
-#[derive(Default, Debug)]
-struct Session {
-    instructions: Vec<(u32, Instruction)>,
-    length_in_bars: u32,
-    finalized: bool,
-}
-
-const BAR_LENGTH: u32 = 24 * 4;
-
-impl Session {
-    fn instructions_this_step(&self, step: u32) -> impl Iterator<Item = Instruction> {
-        assert!(self.finalized);
-        self.instructions
-            .iter()
-            .filter_map(move |(s, inst)| {
-                if step % (BAR_LENGTH * self.length_in_bars) == *s {
-                    Some(inst)
-                } else {
-                    None
-                }
-            })
-            .cloned()
-    }
-
-    fn record_instruction(&mut self, instruction: Instruction, step: u32) {
-        assert!(!self.finalized);
-        match instruction {
-            Instruction::PlayNote {
-                midi_note,
-                len: _,
-                channel_id,
-            } => self.instructions.push((
-                step - 1,
-                Instruction::StopNote {
-                    midi_note,
-                    channel_id,
-                },
-            )),
-            Instruction::StartNote {
-                midi_note,
-                channel_id,
-            } => self.instructions.push((
-                step - 1,
-                Instruction::StopNote {
-                    midi_note,
-                    channel_id,
-                },
-            )),
-            _ => {}
-        }
-        self.instructions.push((step, instruction));
-    }
-
-    fn finalize(&mut self, start_step: u32, end_step: u32) {
-        let start_bar_num = (start_step as f32 / BAR_LENGTH as f32).round() as u32;
-        self.length_in_bars =
-            ((end_step as f32 / BAR_LENGTH as f32).round() as u32 - start_bar_num).max(1);
-        println!(
-            "start bar: {}, length: {}",
-            start_bar_num, self.length_in_bars
-        );
-        self.instructions.iter_mut().for_each(|(s, _)| {
-            let t = *s as i32 - (start_bar_num * BAR_LENGTH) as i32;
-            *s = (t + if t < 0 {
-                (start_bar_num * BAR_LENGTH) as i32
-            } else {
-                0
-            }) as u32;
-        });
-        self.finalized = true;
-    }
-
-    fn is_empty(&self) -> bool {
-        self.instructions.is_empty()
-    }
-
-    fn clear(&mut self) {
-        self.instructions.clear();
-        self.finalized = false;
-        self.length_in_bars = 0;
-    }
-}
+const INPUT_MIDI_CHANNEL: u8 = 10;
 
 struct Looper {
-    sessions: Vec<Session>,
+    sessions: [Vec<Session>; 16],
     current_session: Session,
     record: bool,
     start_step: u32,
+    current_midi_channel: u8,
+    running: bool,
 }
 
 impl Looper {
     fn new() -> Self {
         Looper {
-            sessions: vec![],
+            sessions: Default::default(),
             current_session: Session::default(),
             record: false,
             start_step: 0,
+            current_midi_channel: 1,
+            running: true,
+        }
+    }
+
+    fn update_info(&self) {
+        info!("{}", &self);
+    }
+
+    fn midi_channel_up(&mut self) {
+        self.current_midi_channel = (self.current_midi_channel + 1).min(16);
+    }
+    fn midi_channel_down(&mut self) {
+        self.current_midi_channel = (self.current_midi_channel - 1).max(1);
+    }
+    fn pop_last_session(&mut self) {
+        let idx = (self.current_midi_channel - 1) as usize;
+        if self.record {
+            self.current_session.clear();
+            self.record = false;
+        } else if !self.sessions[idx].is_empty() {
+            self.sessions[idx].pop();
+        }
+    }
+    fn remove_first_session(&mut self) {
+        let idx = (self.current_midi_channel - 1) as usize;
+        if self.sessions[idx].len() > 0 {
+            self.sessions[idx].remove(0);
+        }
+    }
+    fn toggle_recording(&mut self, current_step: u32) {
+        if self.record {
+            // stop recording
+            self.record = false;
+            self.current_session.finalize(self.start_step, current_step);
+            let idx = (self.current_midi_channel - 1) as usize;
+            self.sessions[idx].push(std::mem::take(&mut self.current_session));
+        } else {
+            // start recording
+            self.record = true;
+            self.start_step = current_step;
+        }
+    }
+}
+
+impl Display for Looper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.record {
+            write!(
+                f,
+                "current midi channel: {} -- recording...",
+                self.current_midi_channel
+            )
+        } else {
+            write!(f, "current midi channel: {}", self.current_midi_channel)
         }
     }
 }
@@ -112,27 +91,19 @@ impl mseq::Conductor for Looper {
     }
 
     fn update(&mut self, context: &mut mseq::Context) -> Vec<mseq::Instruction> {
+        if !self.running {
+            context.quit();
+        }
+
         let step = context.get_step();
         let mut instructions = vec![];
-
-        // play rythm
-        // if step % 24 == 0 {
-        //     let vel = if (step / 24) % 4 == 0 { 127 } else { 90 };
-        //     instructions.push(Instruction::PlayNote {
-        //         midi_note: MidiNote {
-        //             note: mseq::Note::C,
-        //             octave: 3,
-        //             vel,
-        //         },
-        //         len: 6,
-        //         channel_id: 2,
-        //     });
-        // }
 
         // play all instructions that play this step
         instructions.extend(
             self.sessions
                 .iter()
+                .map(|midi_session| midi_session.iter())
+                .flatten()
                 .map(|sess| sess.instructions_this_step(step))
                 .flatten(),
         );
@@ -142,22 +113,23 @@ impl mseq::Conductor for Looper {
 
     fn handle_input(&mut self, input: MidiMessage, ctx: &mseq::Context) -> Vec<Instruction> {
         let step = ctx.get_step();
-        println!("midi message: {:?}", input);
+        debug!("midi message: {:?}", input);
+
         match input {
-            MidiMessage::NoteOff { channel, note } => {
+            MidiMessage::NoteOff { channel: _, note } => {
                 if self.record {
                     let inst = Instruction::StopNote {
                         midi_note: note,
-                        channel_id: channel,
+                        channel_id: self.current_midi_channel,
                     };
                     self.current_session.record_instruction(inst, step);
                 }
             }
-            MidiMessage::NoteOn { channel, note } => {
+            MidiMessage::NoteOn { channel: _, note } => {
                 if self.record {
                     let inst = Instruction::StartNote {
                         midi_note: note,
-                        channel_id: channel,
+                        channel_id: self.current_midi_channel,
                     };
                     self.current_session.record_instruction(inst, step);
                 }
@@ -168,42 +140,50 @@ impl mseq::Conductor for Looper {
                 value,
             } => {
                 // pop
-                if channel == 10 && controller == 16 && value > 0 {
-                    if self.current_session.is_empty() {
-                        self.sessions.pop();
-                    } else {
-                        self.current_session.clear();
-                    }
+                if channel == INPUT_MIDI_CHANNEL && controller == 16 && value > 0 {
+                    debug!("pop last session");
+                    self.pop_last_session();
                 }
 
                 // remove first
-                if channel == 10 && controller == 20 && value > 0 {
-                    if self.sessions.len() > 0 {
-                        self.sessions.remove(0);
-                    }
+                if channel == INPUT_MIDI_CHANNEL && controller == 20 && value > 0 {
+                    debug!("remove first session");
+                    self.remove_first_session();
                 }
 
-                // start recording
-                if channel == 10 && controller == 17 && value > 0 {
-                    println!("start recording");
-                    self.record = true;
-                    self.start_step = step;
+                // recording
+                if channel == INPUT_MIDI_CHANNEL && controller == 17 && value > 0 {
+                    debug!("toggle recording");
+                    self.toggle_recording(step);
                 }
-                // stop recording
-                if channel == 10 && controller == 18 && value > 0 {
-                    println!("stop recording");
-                    self.record = false;
-                    self.current_session.finalize(self.start_step, step);
-                    self.sessions
-                        .push(std::mem::take(&mut self.current_session));
+
+                // midi channel down
+                if !self.record && channel == INPUT_MIDI_CHANNEL && controller == 19 && value > 0 {
+                    debug!("midi channel down");
+                    self.midi_channel_down();
                 }
+                // midi channel up
+                if !self.record && channel == INPUT_MIDI_CHANNEL && controller == 23 && value > 0 {
+                    debug!("midi channel up");
+                    self.midi_channel_up();
+                }
+
+                // stop program
+                if channel == INPUT_MIDI_CHANNEL && controller == 22 && value > 0 {
+                    debug!("stop program");
+                    self.running = false;
+                }
+
+                self.update_info();
             }
             _ => {}
         }
 
+        let output = set_note_message_channel(input, self.current_midi_channel);
+
         // passthrough all midi messages
         vec![Instruction::MidiMessage {
-            midi_message: input,
+            midi_message: output,
         }]
     }
 }
@@ -222,7 +202,28 @@ fn main() -> Result<(), MainError> {
         slave: false,
     };
 
+    env_logger::init();
     mseq::run(looper, None, Some(midi_in_params))?;
 
     Ok(())
+}
+
+fn set_note_message_channel(message: MidiMessage, channel: u8) -> MidiMessage {
+    match message {
+        MidiMessage::NoteOff { channel: _, note } => MidiMessage::NoteOff { channel, note },
+        MidiMessage::NoteOn { channel: _, note } => MidiMessage::NoteOn { channel, note },
+        MidiMessage::CC {
+            channel: _,
+            controller: _,
+            value: _,
+        } => message,
+        MidiMessage::PC {
+            channel: _,
+            value: _,
+        } => message,
+        MidiMessage::Clock => message,
+        MidiMessage::Start => message,
+        MidiMessage::Continue => message,
+        MidiMessage::Stop => message,
+    }
 }
---
+++ src/session.rs
@@ -0,0 +1,85 @@
+use log::info;
+use mseq::Instruction;
+
+const BAR_LENGTH: u32 = 24 * 4;
+
+#[derive(Default, Debug)]
+pub struct Session {
+    instructions: Vec<(u32, Instruction)>,
+    length_in_bars: u32,
+    finalized: bool,
+}
+
+impl Session {
+    pub fn instructions_this_step(&self, step: u32) -> impl Iterator<Item = Instruction> {
+        assert!(self.finalized);
+        self.instructions
+            .iter()
+            .filter_map(move |(s, inst)| {
+                if step % (BAR_LENGTH * self.length_in_bars) == *s {
+                    Some(inst)
+                } else {
+                    None
+                }
+            })
+            .cloned()
+    }
+
+    pub fn record_instruction(&mut self, instruction: Instruction, step: u32) {
+        assert!(!self.finalized);
+        match instruction {
+            Instruction::PlayNote {
+                midi_note,
+                len: _,
+                channel_id,
+            } => self.instructions.push((
+                step - 1,
+                Instruction::StopNote {
+                    midi_note,
+                    channel_id,
+                },
+            )),
+            Instruction::StartNote {
+                midi_note,
+                channel_id,
+            } => self.instructions.push((
+                step - 1,
+                Instruction::StopNote {
+                    midi_note,
+                    channel_id,
+                },
+            )),
+            _ => {}
+        }
+        self.instructions.push((step, instruction));
+    }
+
+    pub fn finalize(&mut self, start_step: u32, end_step: u32) {
+        let start_bar_num = (start_step as f32 / BAR_LENGTH as f32).round() as u32;
+        self.length_in_bars =
+            ((end_step as f32 / BAR_LENGTH as f32).round() as u32 - start_bar_num).max(1);
+        info!(
+            "start bar: {}, length: {}",
+            start_bar_num, self.length_in_bars
+        );
+        self.instructions.iter_mut().for_each(|(s, _)| {
+            let t = *s as i32 - (start_bar_num * BAR_LENGTH) as i32;
+            *s = (t + if t < 0 {
+                (start_bar_num * BAR_LENGTH) as i32
+            } else {
+                0
+            }) as u32;
+        });
+        self.finalized = true;
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.instructions.is_empty()
+    }
+
+    pub fn clear(&mut self) {
+        self.instructions.clear();
+        self.finalized = false;
+        self.length_in_bars = 0;
+    }
+}
